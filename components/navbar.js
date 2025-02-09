852d088f9d9714b5eceea97f86108b24a821f8bd
Daniel Chan <djchan@hotmail.ca>
2025-02-09 00:52:43 -0500

    fixed image link

--- components/navbar.js
+++ components/navbar.js
@@ -9,7 +9,7 @@ class Navbar extends HTMLElement {
 <div class="container">
 
   <nav class="navbar navbar-expand-lg navbar-light">
-    <img src="./images/bp_logo.png"">
+    <img src="./images/bp_logo.png">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
