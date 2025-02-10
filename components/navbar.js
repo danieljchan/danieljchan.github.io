d12227398b044de78503eeb8f848da3fd04d97ad
Daniel Chan <djchan@hotmail.ca>
2025-02-10 01:49:38 -0500

    added more links to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -9,27 +9,33 @@ class Navbar extends HTMLElement {
 <div class="container">
 
   <nav class="navbar navbar-expand-lg navbar-light">
-    <img src="./images/bp_logo.png">
+    <img src="./images/bp_logo.png" style="padding-right: 10px;">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto pull-right">
-        <li class="nav-item">
+        <li class="nav-item active">
           <a class="nav-link" href="index.html">Home</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item active">
           <a class="nav-link" href="A000.html">Pricing</a>
         </li>
-        <li class="nav-item dropdown">
+        <li class="nav-item active dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Classes
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="B000.html">Intro</a>
+            <a class="dropdown-item" href="B000.html">Intro to Bouldering</a>
             <a class="dropdown-item" href="C000.html">Kids Team</a>
           </div>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="A000.html">Shop</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="A000.html">Waiver</a>
         </li>
       </ul>
     </div>
